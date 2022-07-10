bda2575a7f99bd8fca6c8698965c5296a3e0a349
kingmingseo <101965138+kingmingseo@users.noreply.github.com>
2022-07-10 23:07:30 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+		
+
+	
+		
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int num1=Integer.parseInt(st.nextToken());
+			int num2=Integer.parseInt(st.nextToken());
+			if(num1==0 && num2 ==0) break;
+			else {
+				bw.write(String.valueOf(num1+num2));
+				bw.newLine();
+		}
+		}
+		
+		
+		
+	
+		bw.close();
+		br.close();
+			
+		
+		
+		
+		
+		
+		}
+}
+

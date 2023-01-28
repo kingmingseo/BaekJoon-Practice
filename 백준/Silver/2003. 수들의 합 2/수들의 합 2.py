82a1e246efb87056c25ae7a1b6db839fb96025e9
kingmingseo <101965138+kingmingseo@users.noreply.github.com>
2023-01-28 14:12:59 +0900

    [Silver IV] Title: 수들의 합 2, Time: 500 ms, Memory: 31620 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,35 +1,26 @@
 import sys
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().split())
+n, m = map(int,input().split())
+data = list(map(int,input().split()))
 
-arr = list(map(int, sys.stdin.readline().split()))
+answer = 0
+start =0
+end = 1
 
-# 구간 초기화
-start, end = 0, 1
+while(start <= end and end <= n):
 
-# m이 되는 경우의 수
-count = 0
+    total = sum(data[start:end])
 
-while (start <= end and end <= n):
+    if (total < m) :
+        end = end +1
 
-    # 구간의 합 계산
-    total = sum(arr[start:end])
+    elif (total > m ):
+        start = start +1
 
-    # 구간의 합이 목푯값보다 작다면
-    if (total < m):
-        # 끝 idx를 오른쪽으로 한 칸 이동
-        end += 1
+    else :
+        answer = answer +1
+        end = end+1
 
-    # 구간의 합이 목푯값보다 크다면
-    elif (total > m):
-        # 시작 idx를 오른쪽으로 한 칸 이동
-        start += 1
 
-    # 구간의 합이 목푯값과 같다면
-    else:
-        # 경우의 수 증가
-        count += 1
-        # 끝 idx를 오른쪽으로 한 칸 이동
-        end += 1
-
-print(count)
+print(answer)

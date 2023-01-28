4a097c1abbfa302bf9c49be6605c8bb42db46acf
kingmingseo <101965138+kingmingseo@users.noreply.github.com>
2023-01-28 16:36:23 +0900

    [level 2] Title: 더 맵게, Time: 1912.60 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,25 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+def solution(scoville, k):
+    count = 0
+    heapq.heapify(scoville)
+
+    while(len(scoville)>0):
+      
+        low_hot_food = heapq.heappop(scoville)
+
+
+        if (low_hot_food < k ):
+            new_food = low_hot_food + ((heapq.heappop(scoville)) * 2)
+            heapq.heappush(scoville, new_food)
+            count = count + 1
+
+
+        if (len(scoville) == 1 and scoville[0] < k):
+            return -1
+
+    return count
+
+print(solution([1, 2, 3, 9, 10, 12], 7))

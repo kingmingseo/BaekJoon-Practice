5c98810232331bba1d20646f414e8bacc52d0e1a
kingmingseo <101965138+kingmingseo@users.noreply.github.com>
2023-05-13 11:02:53 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,25 +1,47 @@
+import sys
+input = sys.stdin.readline
+
 def solution(sequence, k):
-    answers = []
-    sum = 0
-    l = 0
-    r = -1
-    
-    while True:
-        if sum < k:
-            r += 1
-            if r >= len(sequence):
+    start = 0
+    end = -1
+    temp = 0
+    answer =[99999999999,99999999999]
+    while (end < len(sequence)+1):
+        
+        if temp == k :
+            if start >= answer[0] and end - start == answer[1]-answer[0]:
+                temp = temp - sequence[start]
+                start = start + 1
+                continue
+            answer = []
+            answer.append(start)
+            answer.append(end)
+            temp = temp - sequence[start]
+            start = start +1
+
+        elif temp > k :
+            temp = temp - sequence[start]
+            if start >= len(sequence):
                 break
-            sum += sequence[r]
-        else:
-            sum -= sequence[l]
-            if l >= len(sequence):
+            start = start + 1
+
+
+        elif temp < k :
+            end = end + 1
+            if end >= len(sequence):
                 break
-            l += 1
-        if sum == k:
-            answers.append([l, r])
-    
-    answers.sort(key=lambda x: (x[1]-x[0], x[0]))
-    return answers[0]
+            temp = temp + sequence[end]
+
+
+
+    return(answer)
+
+print(solution([1, 1, 1, 2, 3, 4, 5],5))
+
+
+
+
+
 
 
 

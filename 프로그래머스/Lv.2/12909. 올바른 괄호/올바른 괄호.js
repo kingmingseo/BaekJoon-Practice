0ab273325af3b01dd6dae96a6bb2cff463e33d1d
kingmingseo <101965138+kingmingseo@users.noreply.github.com>
2024-01-16 16:20:54 +0900

    [level Lv.2] Title: 올바른 괄호, Time: 2.47 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,21 @@
+function solution(s){
+    var answer = true;
+    let count = 0
+    
+    for (let i =0; i<s.length; i++){
+        if(s[i] === '('){
+            count++ 
+        }
+        else{
+            count--
+        }
+        if (count<0){
+            return false;
+        }
+    }
+    if (count !== 0 ){
+        return false
+    }
+   
+    return answer;
+}

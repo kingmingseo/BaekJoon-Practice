282333aaaf2b9e8c5564d9af01838d10822eeab5
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2024-02-01 19:53:35 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,31 +1,26 @@
 function solution(bridge_length, weight, truck_weights) {
-    var answer = 0;
-    let onBridge = [];
-
-    while (truck_weights.length > 0 || onBridge.length > 0) {
-        // 트럭이 다리에 추가될 수 있는지 확인
-        if (
-            truck_weights.length > 0 &&
-            onBridge.length < bridge_length &&
-            weight >= onBridge.reduce((acc, truck) => acc + truck[0], 0) + truck_weights[0]
-        ) {
-            let nowTruck = truck_weights.shift();
-            onBridge.push([nowTruck, bridge_length]);
+    var answer = 1;
+    let onBridge = [] 
+    
+    while(truck_weights.length > 0 || onBridge.length> 0){
+        let Allweight = onBridge.reduce((a,b)=>a+b[0],0)
+        
+        //다리 위에 올라갈 수 있는 지 판단
+        if(truck_weights.length>0 && bridge_length>onBridge.length && weight >= Allweight+truck_weights[0]){
+            let nowTruck = truck_weights.shift()
+            onBridge.push([nowTruck,bridge_length])
         }
-
-        // 다리 위의 트럭 이동
-        for (let i = 0; i < onBridge.length; i++) {
-            onBridge[i][1] = onBridge[i][1] - 1;
+        
+        //트럭들의 움직임. 만약 거리가 0이되면 다 건넌걸로 간주하고 onBridge에서 제거
+        for(let i=0; i<onBridge.length; i++){
+            onBridge[i][1]=onBridge[i][1]-1;
+            if(onBridge[i][1]===0){
+                onBridge.shift()
+                i--;
+            }
         }
-
-        // 다리를 건넌 트럭 제거
-        while (onBridge.length > 0 && onBridge[0][1] === 0) {
-            onBridge.shift();
-        }
-
-        // 시간 증가
-        answer += 1;
+        answer+=1
+        
     }
-
-    return answer+1;
+    return answer;
 }

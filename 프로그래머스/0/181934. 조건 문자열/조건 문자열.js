c4dc5a620330007611293604bec18a2fcb7ed733
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2024-06-16 21:21:16 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,28 @@
+function solution(ineq, eq, n, m) {
+    var answer = 0;
+    if (ineq == '<'){
+        if (eq == '!'){
+            if ( n < m ){
+                answer=1 
+            }
+        }
+        else{
+            if( n <= m){
+                answer = 1
+            }
+        }
+    }
+    else{
+         if (eq == '!'){
+            if (n > m){
+                answer = 1
+            }
+        }
+        else{
+            if (n>=m){
+                answer = 1
+            }
+        }
+    }
+    return answer;
+}

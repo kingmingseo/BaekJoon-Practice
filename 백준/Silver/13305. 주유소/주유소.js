e5a06f0944fc5d4f64786d340b8a95b93876ba87
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2024-07-14 01:12:10 +0900

    [Silver III] Title: 주유소, Time: 200 ms, Memory: 20076 KB, Score: 17 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -0,0 +1,29 @@
+let [N,...arr] = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
+
+let distance = arr[0].split(' ').map(Number);
+let cityOilFee = arr[1].split(' ').map(Number);
+let answer = 0;
+for(let i=0; i< distance.length; i++){
+  if(i === distance.length -1){
+    answer += distance[i] * cityOilFee[i]
+    break;
+  }
+
+  if(cityOilFee[i]<cityOilFee[i+1]){
+    let skipValue = 1;
+    answer += (cityOilFee[i]*(distance[i]+distance[i+1]))
+    for(let j = i + 2; j < cityOilFee.length-1; j++ ){
+      if(cityOilFee[i]<cityOilFee[j]){
+        answer += (cityOilFee[i]*(distance[j]))
+        skipValue++;
+      }
+    } 
+    i += skipValue 
+    
+  }
+  else if (cityOilFee[i]>=cityOilFee[i+1]){
+    answer += (cityOilFee[i]*(distance[i]))
+  }
+  
+}
+console.log(answer)

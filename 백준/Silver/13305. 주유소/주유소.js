b14c36a1717a94ef4dde17d5bbf237a69564f12d
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2024-07-14 03:01:07 +0900

    [Silver III] Title: 주유소, Time: 292 ms, Memory: 43908 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,32 +1,15 @@
-let [N,...arr] = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
+let [N, ...arr] = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
+let distance = arr[0].split(' ').map(BigInt);
+let cityOilFee = arr[1].split(' ').map(BigInt);
 
-let distance = arr[0].split(' ').map(Number);
-let cityOilFee = arr[1].split(' ').map(Number);
-let answer = 0;
-for(let i=0; i< distance.length; i++){
-  if(i === distance.length -1){
-    answer += distance[i] * cityOilFee[i]
-    break;
-  }
+let answer = BigInt(0);
+let minPrice = cityOilFee[0];
 
-  if(cityOilFee[i]<cityOilFee[i+1]){
-    let skipValue = 1;
-    answer += (cityOilFee[i]*(distance[i]+distance[i+1]))
-    for(let j = i + 2; j < cityOilFee.length-1; j++ ){
-      if(cityOilFee[i]<cityOilFee[j]){
-        answer += (cityOilFee[i]*(distance[j]))
-        skipValue++;
-      }
-      else{
-        break
-      }
-    } 
-    i += skipValue 
-    
-  }
-  else if (cityOilFee[i]>=cityOilFee[i+1]){
-    answer += (cityOilFee[i]*(distance[i]))
-  }
-  
+for (let i = 0; i < distance.length; i++) {
+    if (cityOilFee[i] < minPrice) {
+        minPrice = cityOilFee[i];
+    }
+    answer += minPrice * distance[i];
 }
-console.log(answer)
+
+console.log(answer.toString());

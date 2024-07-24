90ef08b4f7218b325d9ad89566af19ead73ea42c
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2024-07-24 14:52:44 +0900

    [Silver II] Title: 스택 수열, Time: 284 ms, Memory: 32960 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.js
+++ 백준/Silver/1874. 스택 수열/스택 수열.js
@@ -2,33 +2,27 @@ let [N, ...arr] = require('fs').readFileSync('dev/stdin').toString().trim().spli
 N = Number(N);
 arr = arr.map((a) => a.replace('\r', ''));
 arr = arr.map(Number);
+let stackNum = 1;
 let stack = [];
-let tempArray = new Array(N).fill().map((v, i) => i + 1);
-tempArray = tempArray.reverse();
 let answer = '';
-let visited = new Array(N).fill(false);
 
 for (let i = 0; i < N; i++) {
-    if(visited[stack[stack.length-1]] && arr[i]< stack[stack.length-1]){
-        answer ='NO'
-        break;
-    }
-    if (stack.length === 0 || stack[stack.length-1] < arr[i]) {
-        while (tempArray[tempArray.length - 1] !== arr[i]) {
-            let temp = tempArray.pop()
-            stack.push(temp);
-            answer += '+\n';
-            visited[temp-1] = true;
-        }
-        let temp = tempArray.pop()
-        stack.push(temp);
-        answer += '+\n';
-        visited[temp-1] = true;
-    }
-    if (stack[stack.length - 1] === arr[i]) {
-        stack.pop();
-        answer += '-\n';
-    } 
-}
+    let num = arr[i];
 
-console.log(answer.trim())
+    while (stackNum <= num) {
+        stack.push(stackNum);
+        stackNum++;
+        answer += '+\n';
+    }
+
+
+    let popNum = stack.pop();
+    answer += '-\n';
+  
+
+    if (popNum !== num) {
+        answer = 'NO';
+        break
+    }
+}
+console.log(answer.trim());

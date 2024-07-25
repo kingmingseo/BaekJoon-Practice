33a840d9bb80cfedd41b5e179a563a5fae4a20ab
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2024-07-26 02:11:06 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.js
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -1,26 +1,23 @@
 let [N, M] = require('fs').readFileSync('dev/stdin').toString().trim().split(' ').map(Number);
-let arr = new Array(N).fill().map((v, i) => i + 1);
+let visited = new Array(N+1).fill(false);
 let selected = [];
-let answer = '';
-let visited = new Array(N).fill(false);
-
-function dfs(depth, start) {
-    if (depth === M) {
-        for (let item of selected) {
-            answer += `${arr[item]} `;
-        }
-        answer += '\n';
-        return;
+let answer = ''
+function dfs(depth,start){
+  if(depth===M){
+    for(let item of selected){
+      answer+= `${item} `
     }
-    for (let i = start; i < N; i++) {
-        if (visited[i]) continue;
-        visited[i] = true;
-        selected.push(i);
-        dfs(depth + 1, i+1);
-        visited[i] = false;
-        selected.pop();
-    }
+    answer +='\n'
+    return;
+  }
+  for(let i = start; i<=N; i++){
+    if(visited[i]) continue;
+    visited[i] = true;
+    selected.push(i);
+    dfs(depth+1,i);
+    visited[i] = false;
+    selected.pop()
+  }
 }
-
-dfs(0, 0);
-console.log(answer.trim());
+dfs(0,1)
+console.log(answer)

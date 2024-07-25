45ecbc50e0a4c28eb78ead86402e30d0dd10ea22
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2024-07-25 16:02:03 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 9580 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -1,43 +1,17 @@
-let [N, M] = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
+let [N, arr] = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
 N = Number(N);
-let dict = {
-    a: 1,
-    b: 2,
-    c: 3,
-    d: 4,
-    e: 5,
-    f: 6,
-    g: 7,
-    h: 8,
-    i: 9,
-    j: 10,
-    k: 11,
-    l: 12,
-    m: 13,
-    n: 14,
-    o: 15,
-    p: 16,
-    q: 17,
-    r: 18,
-    s: 19,
-    t: 20,
-    u: 21,
-    v: 22,
-    w: 23,
-    x: 24,
-    y: 25,
-    z: 26,
-};
-let total =0;
-let answer = []
 
-for (i = 0; i < M.length; i++) {
-  answer.push(dict[M[i]])
+const r = 31n;
+const M = 1234567891n;
+
+// 3.
+let result = 0n;
+let pow = BigInt(1);
+for (let i = 0; i < N; i++) {
+    result += BigInt(arr[i].charCodeAt(0) - 96) * pow;
+    pow *= r;
 }
 
-for(i=0; i<answer.length; i++){
-  let r = 31**i
-  total += (Number(answer[i])*r) % 1234567891
-}
-
-console.log(total)
+// 4.
+if (result >= M) result %= M;
+console.log(Number(result));

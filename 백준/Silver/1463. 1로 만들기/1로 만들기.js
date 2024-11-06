72aa2744d6a93eb5f3a5abbba7cd437f1ea647fd
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2024-11-06 10:53:05 +0900

    [Silver III] Title: 1로 만들기, Time: 176 ms, Memory: 17596 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -1,16 +1,19 @@
-const fs = require('fs');
-const input = Number(fs.readFileSync('dev/stdin').toString().trim());
-let d = new Array(100000).fill(0)
+let input = require('fs').readFileSync('dev/stdin').toString().trim();
+input = Number(input);
+let dpTable = new Array(input+1).fill(0);
 
-for(let i=2; i<=input; i++){
-    d[i] = d[i-1] + 1;
 
-    if(i%2 ===0){
-        d[i] = Math.min(d[i],d[parseInt(i/2)]+1)
+//dpTable의 인덱스 i를 1로 만드는데 소모하는 횟수
+for (let i = 2; i <= input; i++) {
+    dpTable[i] = dpTable[i-1] + 1
+
+    if (i% 2 === 0) {
+        dpTable[i] = Math.min(dpTable[i],dpTable[parseInt(i/2)]+1);
     }
+
     if(i%3 ===0){
-        d[i] = Math.min(d[i],d[parseInt(i/3)]+1)
+        dpTable[i] = Math.min(dpTable[i],dpTable[parseInt(i/3)]+1);
     }
 }
 
-console.log(d[input]);
+console.log(dpTable[input])

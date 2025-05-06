846586491572f8b3b7ba85fa1d7e1bfc0b2efaf8
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-05-06 18:24:46 +0900

    [Bronze III] Title: 최소, 최대, Time: 416 ms, Memory: 93584 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.js
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -1,15 +1,3 @@
-let [n, ...arr] = require('fs').readFileSync('dev/stdin').toString().split('\n');
-arr = arr[0].split(' ')
-let max= arr[0];
-let min = arr[0];
-
-for(let item of arr){
-  if (max < Number(item)){
-    max = Number(item)
-  }
-  if (min > Number(item)){
-    min = Number(item)
-  }
-}
-
-console.log(min,max)
+const input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
+const data = input[1].split(' ').map(number => Number(number))
+console.log(Math.min(...data), Math.max(...data))

cb8508cc23e0d6c7988d470f4c10f78051a94ba1
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-05-06 17:54:39 +0900

    [Bronze V] Title: 합, Time: 88 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.js
+++ 백준/Bronze/8393. 합/합.js
@@ -1,6 +1,9 @@
-let input = require('fs').readFileSync('dev/stdin').toString();
-let answer = 0
-for(let i =1; i<=input; i++){
-  answer+=i
+const input = require('fs').readFileSync('dev/stdin').toString().trim()
+
+let answer = 0;
+
+for(let i=1; i<=input; i++){
+  answer += i
 }
+
 console.log(answer)

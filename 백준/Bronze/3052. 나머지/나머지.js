73c3fc35fd1ce5249b9713b5783680bc4e99614f
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-05-06 18:34:53 +0900

    [Bronze II] Title: 나머지, Time: 88 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.js
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -1,9 +1,8 @@
-let input = require('fs').readFileSync('dev/stdin').toString().split('\n');
-input = input.map(Number)
-let temp = new Set()
+const input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n').map(line => line.trim()).map(string => Number(string))
+let dataSet = new Set()
 
-for (let i = 0; i<10; i++){
-  temp.add(input[i]%42)
+for (let i = 0; i < input.length; i++) {
+  dataSet.add(input[i] % 42)
 }
 
-console.log(temp.size)
+console.log(dataSet.size)

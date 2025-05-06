5a0c74fa652c878998c5b83c682001fa210d0032
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-05-06 18:30:48 +0900

    [Bronze III] Title: 최댓값, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.js
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -1,11 +1,13 @@
-let input = require('fs').readFileSync('dev/stdin').toString().split('\n')
-let max = -1
-let number = 0
-for (let i=1; i<=9; i++){
-  if(max<Number(input[i-1])){
-    number = i;
-    max = Number(input[i-1])
+const input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n').map(line => line.trim())
+let transformData = input.map(num => Number(num))
+let candidateMaxVal = -Infinity
+let candidateMaxValIdx = 0
+for (let i = 0; i < transformData.length; i++) {
+  if (candidateMaxVal < transformData[i]) {
+    candidateMaxVal = transformData[i]
+    candidateMaxValIdx = i
   }
 }
-console.log(`${max}
-${number}`)
+
+console.log(candidateMaxVal)
+console.log(candidateMaxValIdx+1)

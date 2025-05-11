bf39793214226b9a571915a6631abf70c0e23ab1
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-05-12 01:49:46 +0900

    [Bronze II] Title: 수 정렬하기, Time: 144 ms, Memory: 9972 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -1,19 +1,19 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().split('\n').filter(Boolean).map(Number);
+let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n').map(Number)
 
 function selectionSort(arr){
-  for(let i = 0; i < arr.length; i++){
-    let minIndex = i;
-    for(let j = i + 1; j < arr.length; j++){
-      if(arr[j] < arr[minIndex]){
-        minIndex = j;
+  for(let i=0; i<arr.length; i++){
+    let minIndex = i
+
+    for(let j= i+1; j<arr.length; j++){
+      if(arr[j]<=arr[minIndex]){
+        minIndex = j
       }
     }
-    let temp = arr[i];
-    arr[i] = arr[minIndex];
-    arr[minIndex] = temp;
+    let temp = arr[i]
+    arr[i] = arr[minIndex]
+    arr[minIndex] = temp 
   }
 }
-
-const data = input.slice(1); // 첫 줄은 개수
-selectionSort(data);
-console.log(data.join('\n'));
+input = input.slice(1)
+selectionSort(input)
+console.log(input.join('\n').trim())

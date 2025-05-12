c062badb897870d542dc75fee247f7227946f0b8
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-05-12 10:50:21 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1200 ms, Memory: 216296 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -1,4 +1,5 @@
-let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n').map(a=>Number(a));
-input = input.slice(1)
-let sortArr = input.sort((a,b)=>a-b);
-console.log(sortArr.join('\n'))
+const input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n').map(Number)
+let data = input.slice(1)
+
+data = data.sort((a,b)=>a-b)
+console.log(data.join('\n').trim())

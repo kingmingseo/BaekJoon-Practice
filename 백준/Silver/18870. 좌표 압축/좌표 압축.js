bec68facb30c015bcbd7cc88a9e4fc3655fd3843
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-05-12 16:38:38 +0900

    [Silver II] Title: 좌표 압축, Time: 1988 ms, Memory: 327128 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.js
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.js
@@ -1,16 +1,18 @@
-let [n,arr]= require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
-arr = arr.split(' ')
-let data =[...arr]
-data.sort((a,b)=>a-b)
-let dupDelArr = [...new Set(data)];
+let [N, arr] = require('fs').readFileSync('dev/stdin').toString().split('\n')
+arr = arr.split(' ').map(Number)
+let set = new Set(arr)
+let data = [...set]
+
 let dic = {};
-let answer = ''
-for(let i=0; i<dupDelArr.length; i++){
-  dic[dupDelArr[i]]=i
+let temp = [...data].sort((a,b)=>a-b)
+let result = ''
+
+for(let i =0; i<temp.length; i++){
+  dic[temp[i]] = i
 }
 
-for(let i=0; i<n; i++){
-  answer += dic[arr[i]]+' '
+for(let i=0; i<N; i++){
+  result += dic[arr[i]]+' '
 }
 
-console.log(answer.trim())
+console.log(result.trim())

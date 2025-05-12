2a8a759a9a739e289063072830d7bc4bc918b51b
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-05-12 22:21:26 +0900

    [Silver V] Title: 소트인사이드, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.js
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -1,3 +1,4 @@
-let input = require('fs').readFileSync('dev/stdin').toString().trim().split('').map(Number)
+const input = require('fs').readFileSync('dev/stdin').toString().trim().split('').map(Number)
+input.sort((a,b)=>b-a)
 
-console.log(input.sort((a,b)=>b-a).join(''))
+console.log(input.join(''))

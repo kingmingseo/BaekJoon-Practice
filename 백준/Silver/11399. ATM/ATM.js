d8bf7d23fffd503f872e4cc40114ec09e9b73cc2
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-05-13 15:47:37 +0900

    [Silver IV] Title: ATM, Time: 100 ms, Memory: 9648 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.js
+++ 백준/Silver/11399. ATM/ATM.js
@@ -1,11 +1,11 @@
-let [n,...arr] = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
-arr = arr[0].split(' ').map(Number)
-answer = 0
-arr.sort((a,b)=>a-b)
+let [N,P] = require('fs').readFileSync('dev/stdin').toString().split('\n')
+P = P.split(' ').map(Number).sort((a,b)=>a-b)
 
-for(let i=0; i<arr.length; i++){
-  for(let j=0; j<=i; j++){
-    answer += arr[j]
-  }
+let waitTime = 0
+let answer = 0
+for(let i =0; i<P.length; i++){
+  waitTime += P[i]
+  answer += waitTime
 }
+
 console.log(answer)

9d7dadb149f07bb3063831b11d22c77ebcbd8ee9
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-06 00:08:45 +0900

    [Bronze V] Title: 구구단, Time: 88 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.js
+++ 백준/Bronze/2739. 구구단/구구단.js
@@ -1,8 +1,7 @@
-const input = require('fs').readFileSync('dev/stdin').toString().trim()
-
-let answer = ''
-for (let i = 1; i < 10; i++) {
-  answer += `${input} * ${i} = ${Number(input) * i}\n`
+let input =require('fs').readFileSync('dev/stdin').toString()
+let answer =''
+for(let i=1; i<10; i++){
+    answer += `${Number(input)} * ${i} = ${Number(input)*i}\n`
 }
 
 console.log(answer.trim())

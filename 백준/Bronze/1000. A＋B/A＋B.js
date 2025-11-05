5f6ccace757dd9239c69d356ef7c6309b050ced2
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-05 14:10:44 +0900

    [Bronze V] Title: A+B, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,6 +1,4 @@
-const input = require('fs').readFileSync('dev/stdin').toString().split(' ')
-
-let a = Number(input[0])
-let b = Number(input[1])
-
-console.log(a+b)
+let input = require('fs').readFileSync('dev/stdin').toString().trim().split(' ')
+let A = Number(input[0])
+let B = Number(input[1])
+console.log(A+B)

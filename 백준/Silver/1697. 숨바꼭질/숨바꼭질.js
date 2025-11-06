3d41fbe973ce2024dd4bc9594dc96af7af3e1bb1
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-06 14:12:31 +0900

    [Silver I] Title: 숨바꼭질, Time: 3832 ms, Memory: 20656 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -2,33 +2,31 @@ let [N, K] = require('fs').readFileSync('dev/stdin').toString().trim().split(' '
 N = Number(N)
 K = Number(K)
 
-let visited = Array(100000).fill(false)
+let visited = Array(100001).fill(false)
 let queue = []
-let total = 0
-function bfs(v, target) {
-    queue.push([v, 0])
-    visited[v] = true
+
+function bfs(start, target) {
+    queue.push([start, 0])
+    visited[start] = true
 
     while (queue.length !== 0) {
         let [now, dist] = queue.shift()
+
         if (now === target) {
             return dist
         }
 
-        let nextPositions = [now - 1, now + 1, now * 2];
+        let nextPosition = [now + 1, now - 1, now * 2]
 
-        for (let next of nextPositions) {
-            if (next >= 0 && next <= 100000 && !visited[next]) {
-                queue.push([next, dist + 1]);
-                visited[next] = true;
+        for (let next of nextPosition) {
+            if (next >= 0 && next <= 100000 && visited[next] === false) {
+                queue.push([next, dist + 1])
+                visited[next] = true
             }
+
         }
 
     }
-
 }
-
-
-
 
 console.log(bfs(N, K))

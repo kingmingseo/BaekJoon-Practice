1fd87c3e188e8f69271b5e92a3190033efd4f6c0
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-06 10:56:44 +0900

    [Bronze II] Title: 문자열 반복, Time: 92 ms, Memory: 9356 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.js
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -1,12 +1,22 @@
-let [n,...arr] = require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
+let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
 
-
-for(let i=0; i<n; i++){
-  let answer = ''
-  let temp= arr[i].split(' ')
-  temp[1]= temp[1].replace('\r','')
-  for(let j of temp[1]){
-    answer += j.repeat(temp[0])
-  }
-  console.log(answer)
+let data = []
+for(let i=1; i<input.length; i++){
+    let temp=input[i].split(' ')
+    temp[0] = Number(temp[0])
+    temp[1] = temp[1].replace('\r','')
+    data.push(temp)
 }
+
+for(let i=0; i<data.length; i++){
+    let output = ''
+    for(let j=0; j<data[i][1].length; j++){
+        output+= data[i][1][j].repeat(data[i][0])
+    }
+    console.log(output)
+  
+}
+
+
+
+

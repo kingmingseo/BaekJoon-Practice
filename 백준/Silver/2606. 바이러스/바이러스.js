2349ba0911aea48062c4d28ff8160f4e0b190cd8
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-06 13:07:37 +0900

    [Silver III] Title: 바이러스, Time: 88 ms, Memory: 9416 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,33 +1,30 @@
-let [N, M, ...arr] = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
-N = Number(N);
-M = Number(M);
-arr = arr.map((a) => a.split(' ').map(Number));
-let graph = [[]];
-for (let i = 0; i <= 100; i++) {
-    graph.push([]);
+let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
+let computer = Number(input[0])
+let pairCount = Number(input[1])
+let data = Array.from({ length: computer + 1 }, () => [])
+
+//원하는 데이터 구조 만들기
+for (let i = 2; i < input.length; i++) {
+    let temp = input[i].split(' ').map(Number)
+    data[temp[0]].push(temp[1])
+    data[temp[1]].push(temp[0])
 }
 
-for (let i = 0; i < arr.length; i++) {
-    let nowCom = arr[i][0];
-    let connectCom = arr[i][1];
-    graph[connectCom].push(nowCom);
-    graph[nowCom].push(connectCom);
-}
 
-let visited = new Array(N + 1).fill(false);
-
-function dfs(nowCom) {
-    for (let item of graph[nowCom]) {
-        if (visited[item]) continue;
-        visited[item] = true;
-        dfs(item);
+let visited = Array(computer + 1).fill(0);
+function dfs(graph, v, visited) {
+    visited[v] = 1
+    for (let i of graph[v]) {
+        if (visited[i] === 0) {
+            dfs(graph, i, visited)
+        }
     }
 }
-dfs(1);
 
-const trueCount = visited.filter((value) => value === true).length;
-if (trueCount === 0) {
-    console.log(0);
-} else {
-    console.log(trueCount - 1);
-}
+dfs(data, 1, visited)
+
+let total = visited.reduce((acc, cur) => {
+    return acc + cur
+}, 0)
+
+console.log(total - 1)

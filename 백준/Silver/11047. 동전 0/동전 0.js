fa488416952782a33bacf10b1cf76b5ce03ff286
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-11 21:28:24 +0900

    [Silver IV] Title: 동전 0, Time: 92 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -1,17 +1,12 @@
-const [input, ...data] = require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
-
-const [N, K] = input.split(' ').map(Number)
-
-/* 동전 가치 데이터를 가장 큰 것 부터 한 번씩 순회하면서 나눠봐야함 */
-/* 나눈 몫을 기록해야하고 나머지에 대해서 또 나눗셈을 수행해야함 (0이 될때 까지) */
-
-let result = K;
-let count = 0;
-for (let i = N - 1; i >= 0; i--) {
-  let nowCoinValue = data[i]
-  let needCoin = Math.floor(result / nowCoinValue);
-  count += needCoin
-  result = result - (nowCoinValue * needCoin)
+let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
+let [N, total] = input[0].split(' ').map(Number)
+let data = input.slice(1).map(Number)
+let ans = 0
+for (let i=data.length-1; i>=0; i--) {
+    let coin = data[i]
+    let useCoin = Math.floor(total / coin)
+    ans += useCoin
+    total = total - (useCoin * coin)
 }
 
-console.log(count)
+console.log(ans)

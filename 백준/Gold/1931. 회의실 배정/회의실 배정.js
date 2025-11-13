7f27b2122f674c72712f6e80aa194df64947d27a
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-13 19:37:50 +0900

    [Gold V] Title: 회의실 배정, Time: 332 ms, Memory: 41736 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.js
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.js
@@ -1,24 +1,23 @@
 let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
-let data = input.slice(1)
-data = data.map(i => i.split(' ').map(Number))
+let data = input.slice(1).map(a => a.split(' ').map(Number))
 
 function compare(a, b) {
-  if (a[1]===b[1]){
-    return a[0]-b[0]
-  }
-  return a[1] - b[1]
+    if (a[1] === b[1]){
+        return a[0] - b[0]
+    }
+    return a[1] - b[1]
 }
-
 data.sort(compare)
-let minEndTime = -Infinity;
-let answer = 0
+let finishTime = -1
+let ans = 0
 
 for (let i = 0; i < data.length; i++) {
-  let nowStartTime = data[i][0]
-  if (nowStartTime >= minEndTime) {
-    answer += 1
-    minEndTime = data[i][1]
-  }
+
+    let startTime = data[i][0] // 이번 회의 시작시간
+    if (finishTime <= startTime) { //이번 회의 시작시간이 이전 회의 끝난 시간과 같거나 더 나중일때
+        ans++
+        finishTime = data[i][1] // 이번회의 끝나는 시간으로 변수 교체 
+    }
 }
 
-console.log(answer)
+console.log(ans)

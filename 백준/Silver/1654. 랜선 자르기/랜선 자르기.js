c70706acc5cc8226e4e33d78db7fee511e50d6e5
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-16 17:04:02 +0900

    [Silver II] Title: 랜선 자르기, Time: 172 ms, Memory: 13564 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
@@ -1,23 +1,23 @@
-let [temp, ...arr] = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
-let [K, N] = temp.split(' ').map(Number);
-arr = arr.map(Number);
-let result = 0;
+let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
+let [K, N] = input[0].split(' ').map(Number)
+let data = input.slice(1).map(Number)
 let start = 1;
-let end = arr.reduce((a, b) => Math.max(a, b));
-
+let end = Math.max(...data)
+let result = 0;
 while (start <= end) {
-    let mid = Math.floor((start + end) / 2);
-    let total = 0;
-
-    for (let x of arr) {
-        total += Math.floor(x / mid);
+    let mid = Math.floor((start + end) / 2)  //자르는 랜선의 공통 길이
+    let total = 0
+    for (let x of data) {
+        total += Math.floor(x / mid) //mid의 길이로 잘랐을 때 만들 수 있는 랜선의 개수
     }
 
-    if (total >= N) {
+    if (total < N) { //자르고 나온 랜선의 개수가 필요한 랜선의 개수보다 적을때(즉 랜선이 더 짧아야함)
+        end = mid - 1
+    }
+    else { //랜선의 개수가 너무 많거나 같을때 (즉 랜선이 더 길어야함)
         result = mid;
-        start = mid + 1;
-    } else {
-        end = mid - 1;
+        start = mid + 1
     }
 }
-console.log(result);
+
+console.log(result)

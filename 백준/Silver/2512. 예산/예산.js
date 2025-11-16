44530d3e84a421282ccb841233e02057a542c22d
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-16 16:00:25 +0900

    [Silver II] Title: 예산, Time: 160 ms, Memory: 11924 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.js
+++ 백준/Silver/2512. 예산/예산.js
@@ -1,26 +1,25 @@
-let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
-let [N,arr,M] = input
-N= Number(N);
-arr = arr.split(' ').map(Number);
-M = Number(M);
+let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
+let N = Number(input[0])
+let data = input[1].split(' ').map(Number)
+let budget = Number(input[2])
+let end = Math.max(...data)
+let start = 1
+let result = 0
 
-let end = arr.reduce((a,b)=>Math.max(a,b))
-let start = 1;
-let result = 0;
+while (start <= end) {
+    let mid = Math.floor((start + end) / 2)
+    let total = 0
+    for (let x of data) {
+        total += Math.min(x, mid)
+    }
 
-while(start<=end){
-  let mid = Math.floor((start+end)/2);
-  let total = 0;
-  for(let x of arr){
-    total += Math.min(mid,x);
-  }
-  if(total<=M){
-    result = mid
-    start = mid+1
-  }
-  else{
-    end = mid-1;
-  }
+    if (total > budget) {
+        end = mid - 1
+    }
+    else {
+        result = mid
+        start = mid + 1
+    }
 }
 
 console.log(result)

064a922d9fd6919839104d83add0f67a22c28c2e
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-16 16:48:51 +0900

    [Silver II] Title: 나무 자르기, Time: 664 ms, Memory: 141204 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.js
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.js
@@ -1,26 +1,30 @@
-let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
-let [N, M] = input[0].split(' ').map(Number);
-let arr = input[1].split(' ').map(Number);
-
-let start = 1;
-let end = arr.reduce((a, b) => Math.max(a, b));
-let result = 0;
-
+let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
+let [N, M] = input[0].split(' ').map(Number)
+let data = input[1].split(' ').map(Number)
+let start = 0;
+let end = Math.max(...data)
+let result;
 while (start <= end) {
-    let total = 0;
-    let mid = Math.floor((start + end) / 2);
-    for (let x of arr) {
-        if (x < mid) {
-            continue;
+    let mid = Math.floor((start + end) / 2) //절단기 높이 설정
+    let total = 0 // 가져가는 나무 
+
+    for (let x of data) {
+        let tree = x - mid
+        if (tree > 0) {
+            total += tree
         }
-        total += x - mid;
     }
+   
+    //가져 가는 나무가 적을때 (절단기 높이를 낮춰야함)
     if (total < M) {
-        end = mid - 1;
-    } else {
-        result = mid;
-        start = mid + 1;
+        end = mid - 1
+
+    }
+    //가져가는 나무가 너무 많거나 같을때 (절단기 높이를 높여야함)
+    else {
+        result = mid
+        start = mid + 1
     }
 }
 
-console.log(result);
+console.log(result)

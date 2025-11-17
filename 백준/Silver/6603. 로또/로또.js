7e6ce6b5458ff6f62fa8ae9f0d0fdd145e893afe
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-17 17:54:39 +0900

    [Silver II] Title: 로또, Time: 152 ms, Memory: 10516 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.js
+++ 백준/Silver/6603. 로또/로또.js
@@ -1,45 +1,38 @@
-let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
-let data = [];
-input = input.map((a) => a.split(' ').map(Number));
-let visited = new Array(50).fill(false);
-let selected = [];
-let answer = '';
+let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
+
+input = input.slice(0, input.length - 1)
+
+let data = []
+let nowData
+let visited = []
 for (let i = 0; i < input.length; i++) {
-    let temp = [];
-    for (let j = 1; j < input[i].length; j++) {
-        temp.push(input[i][j]);
-    }
-    data.push(temp);
+    let tempData = input[i].split(' ').slice(1).map(Number)
+
+    data.push(tempData)
 }
 
-
-function dfs(depth, length, j) {
-    //모아진 순열을 answer에 담는 부분
+let ans = ''
+function dfs(result, depth) {
     if (depth === 6) {
-        for (let item of selected) {
-            answer += `${item} `;
-        }
-        answer += `\n`;
-        return;
+        ans += result.join(' ')
+        ans += '\n'
+        return
     }
-    //순열을 만들기 위해 재귀 시키는 부분
-    for (let i = 0; i < length; i++) {
-        if (visited[data[j][i]]) continue;
-        if (selected[selected.length - 1] > data[j][i]) {
-            visited[data[j][i]] = false;
-            continue
-        }
-        visited[data[j][i]] = true;
-        selected.push(data[j][i]);
-        dfs(depth + 1, length, j);
-        visited[data[j][i]] = false;
-        selected.pop();
+
+    for (let i = 0; i < nowData.length; i++) {
+        if (visited[i] || nowData[i] < result[result.length - 1]) continue
+        visited[i] = true
+        result.push(nowData[i])
+        dfs(result, depth + 1)
+        result.pop()
+        visited[i] = false
     }
 }
 
-//여러 테스트 케이스를 시도하는 부분
-for (let i = 0; i < data.length - 1; i++) {
-    dfs(0, data[i].length, i);
-    console.log(answer.trim()+'\n');
-    answer = '';
+for (let item of data) {
+    nowData = item
+    visited = Array(nowData.length).fill(false)
+    dfs([], 0)
+    ans += '\n'
 }
+console.log(ans.trim())

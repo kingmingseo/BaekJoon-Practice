9eca5fb8305c699ebe53d193d6735a65fcce940b
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-19 22:15:27 +0900

    [Silver I] Title: 포도주 시식, Time: 96 ms, Memory: 11612 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.js
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.js
@@ -1,18 +1,16 @@
 let [N, ...arr] = require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
 arr = arr.map(Number)
-
-N = Number(N)
-let d = new Array(N).fill(0)
-
-d[0] = arr[0]
-d[1] = arr[1] + arr[0]
-d[2] = Math.max(arr[0] + arr[2], arr[0] + arr[1], arr[1] + arr[2])
+let dp = Array(N).fill(0)
+dp[0] = arr[0]
+dp[1] = arr[0] + arr[1]
+dp[2] = Math.max(arr[0] + arr[1], arr[0] + arr[2], arr[1] + arr[2])
 
 for (let i = 3; i < N; i++) {
-    d[i] = d[i - 1]
-    d[i] = Math.max(d[i], arr[i] + d[i - 2])
-    d[i] = Math.max(d[i], arr[i - 1] + d[i - 3] + arr[i])
+    let candidate1 = dp[i - 1]
+    let candidate2 = arr[i] + dp[i - 2]
+    let candidate3 = arr[i] + arr[i - 1] + dp[i - 3]
 
+    dp[i] = Math.max(candidate1, candidate2, candidate3)
 }
 
-console.log(d[N - 1])
+console.log(dp[N - 1])

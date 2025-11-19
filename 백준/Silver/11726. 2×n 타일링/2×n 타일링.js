0a3741855229b3e592ff7d5886ff1276665c30fd
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-19 23:33:18 +0900

    [Silver III] Title: 2×n 타일링, Time: 144 ms, Memory: 9644 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
@@ -0,0 +1,12 @@
+let input = Number(require('fs').readFileSync('dev/stdin').toString().trim())
+let dp = []
+
+dp[0] = 0
+dp[1] = 1
+dp[2] = 2
+
+for (let i = 3; i <= input; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2])%10007
+}
+
+console.log(dp[input])

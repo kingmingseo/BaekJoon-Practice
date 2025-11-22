a003e0931d56ca265c89a11c896d33ca62d7f7fb
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-22 22:42:37 +0900

    [Silver II] Title: 유기농 배추, Time: 168 ms, Memory: 13260 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.js
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -1,63 +1,56 @@
-let [T, ...arr] = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
-T = Number(T);
-arr = arr.map((a) => a.split(' ').map(Number));
+const { join } = require('path');
 
-let nowIndex = 0;
-let testCase = [];
-let testCase2 = [];
-for (let i = 0; i < T; i++) {
-    let repeat = arr[nowIndex][2];
-    testCase2.push(arr[nowIndex]);
-    let temp = [];
-    for (let j = 0; j < repeat; j++) {
-        nowIndex++;
-        temp.push(arr[nowIndex]);
+let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
+input = input.map(data => data.replace('\r', '')).map(data => data.split(' ').map(Number))
+let N = input[0][0]
+let nowRow = 1;
+
+
+for (let i = 0; i < N; i++) {
+
+    //각 테스트 케이스를 위해서 그래프를 만들기 
+    let [col, row, loopLimit] = input[nowRow]
+    let loopCount = 0
+    let ans = 0
+    let graph = Array.from({ length: row }, () => Array(col).fill(0))
+    var visited = Array.from({ length: row }, () => Array(col).fill(false))
+    while (loopCount < loopLimit) {
+        nowRow += 1
+
+        let [col, row] = input[nowRow]
+
+        graph[row][col] = 1
+
+
+        loopCount += 1
     }
-    nowIndex++;
-    testCase.push(temp);
-}
 
-function makeGraph(x, y, data) {
-    let graph = [];
-    for (let i = 0; i < y; i++) {
-        let temp = new Array(x).fill(0);
-        graph.push(temp);
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            if (visited[i][j]) continue
+            if (graph[i][j] === 0) continue
+            visited[i][j] = true
+            ans++
+            dfs(visited, i, j, col, row, graph)
+
+        }
     }
-    for (let item of data) {
-        graph[item[1]][item[0]] = 1;
-    }
-    return graph
-}
 
-
-
-function dfs(x, y, graph) {
-    if (x < 0 || x > graph[0].length-1) return;
-    if (y < 0 || y > graph.length-1) return;
-
-    if (graph[y][x] == 1) {
-        graph[y][x] = -1;
-        dfs(x + 1, y, graph);
-        dfs(x - 1, y, graph);
-        dfs(x, y + 1, graph);
-        dfs(x, y - 1, graph);
-        return true;
-    }
-    return false;
+    console.log(ans)
+    nowRow += 1
 }
 
 
-for(let i=0; i<T; i++){
-  let nowCase = testCase[i]
-  let nowX = testCase2[i][0]
-  let nowY = testCase2[i][1]
-  let answer = 0;
-  graph = makeGraph(nowX,nowY,nowCase)
-
-  for(let y=0; y<nowY; y++){
-    for(let x=0; x<nowX; x++){
-      if(dfs(x,y,graph)) answer++
+function dfs(visited, nowRow, nowCol, col, row, graph) {
+    let dx = [1, -1, 0, 0]
+    let dy = [0, 0, 1, -1]
+    for (let i = 0; i < 4; i++) {
+        let nextCol = nowCol + dx[i]
+        let nextRow = nowRow + dy[i]
+        if (nextRow < 0 || nextCol < 0 || nextRow >= row || nextCol >= col) continue
+        if (visited[nextRow][nextCol]) continue
+        if (graph[nextRow][nextCol] === 0) continue
+        visited[nextRow][nextCol] = true
+        dfs(visited, nextRow, nextCol, col, row, graph)
     }
-  }
-  console.log(answer)
 }

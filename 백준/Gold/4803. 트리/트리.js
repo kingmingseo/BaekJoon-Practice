ca9c18cd7c04728824e5554d8352bc68589ddb81
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-23 22:06:04 +0900

    [Gold IV] Title: 트리, Time: 468 ms, Memory: 73572 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.js
+++ 백준/Gold/4803. 트리/트리.js
@@ -1,48 +1,61 @@
-let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
-input = input.map((a) => a.split(' ').map(Number));
-let nowIndex = 0;
-let graphInfo = [];
-let graph = []
+let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
+input = input.map(data => data.replace('\r', ''))
+input = input.map(data => data.split(' ').map(Number))
 
-while (true) {
-    let [point, line] = input[nowIndex];
-    graphInfo.push([point, line]);
-    if (point === 0 && line === 0) break;
-    nowIndex += 1;
-    let graphTemp = Array.from({ length: point + 1 }, () => []);
-    for (let i = 0; i < line; i++) {
-        let [startNode, endNode] = input[nowIndex];
-        graphTemp[startNode].push(endNode);
-        graphTemp[endNode].push(startNode);
-        nowIndex++;
+let nowRow = 0;
+let testCase = 1
+while (nowRow < input.length - 1) {
+    let [n, m] = input[nowRow]
+    nowRow++
+
+    //그래프 만들기
+    let graph = Array.from({ length: n + 1 }, () => [])
+    let testCaseRow = 0;
+    while (testCaseRow < m) {
+        let [startNode, endNode] = input[nowRow]
+
+        graph[startNode].push(endNode)
+        graph[endNode].push(startNode)
+        nowRow++
+        testCaseRow++
     }
-    graph.push(graphTemp)
+
+    let treeCnt = 0
+    let visited = Array(n + 1).fill(false)
+    for (let i = 1; i <= n; i++) {
+        if (visited[i]) continue
+        visited[i] = true
+        var hasCycle = false
+        dfs(graph, i, 0, visited)
+        if (!hasCycle) treeCnt++
+    }
+
+
+    if (treeCnt === 0) {
+        console.log(`Case ${testCase}: No trees.`)
+    }
+    else if (treeCnt === 1) {
+        console.log(`Case ${testCase}: There is one tree.`)
+    }
+    else {
+        console.log(`Case ${testCase}: A forest of ${treeCnt} trees.`)
+    }
+    testCase++
+
+
 }
 
-function isCycle(x, prev,graph) {
-    visited[x] = true;
-    
-    for (let item of graph[x]) {
-        if(!visited[item]){
-            if(isCycle(item,x,graph)) return true;
-        }
-        else if (item!=prev) return true;
-    }
-    return false; 
-    
-}
 
-for (let i=0; i<graph.length; i++) {
-    let pointCount = graphInfo[i][0]
-    visited = new Array(pointCount + 1).fill(false);
-    let answer = 0
-    let NowGraph = graph[i]
-    for(let i=1; i<NowGraph.length; i++){
-        if(!visited[i]){
-            if(!isCycle(i,0,NowGraph)) answer+=1;
+function dfs(graph, nowNode, prevNode, visited) {
+    for (let nextNode of graph[nowNode]) {
+        if (prevNode === nextNode) continue
+        if (visited[nextNode]) {
+            hasCycle = true
+            break
         }
+        visited[nextNode] = true
+        dfs(graph, nextNode, nowNode, visited)
     }
-    if(answer>1){console.log(`Case ${i+1}: A forest of ${answer} trees.`)}
-    else if(answer===1){console.log(`Case ${i+1}: There is one tree.`)}
-    else {console.log(`Case ${i+1}: No trees.`)}
+
+
 }

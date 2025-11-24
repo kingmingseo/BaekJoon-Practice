d79ea5bb4575d100733a978e46a2a668110423bf
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-24 11:53:23 +0900

    [Gold IV] Title: 트리, Time: 464 ms, Memory: 73172 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.js
+++ 백준/Gold/4803. 트리/트리.js
@@ -1,61 +1,59 @@
 let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
-input = input.map(data => data.replace('\r', ''))
-input = input.map(data => data.split(' ').map(Number))
-
+input = input.map(data => data.replace('\r', '').split(' ').map(Number))
 let nowRow = 0;
-let testCase = 1
+let testNum = 1
+
 while (nowRow < input.length - 1) {
     let [n, m] = input[nowRow]
-    nowRow++
 
-    //그래프 만들기
-    let graph = Array.from({ length: n + 1 }, () => [])
-    let testCaseRow = 0;
-    while (testCaseRow < m) {
+    var graph = Array.from({ length: n + 1 }, () => [])
+    var visited = Array(n + 1).fill(false)
+    nowRow++
+    // 그래프 만들기
+    for (let i = 0; i < m; i++) {
         let [startNode, endNode] = input[nowRow]
 
         graph[startNode].push(endNode)
         graph[endNode].push(startNode)
         nowRow++
-        testCaseRow++
     }
 
+
+    //방문하기
+
     let treeCnt = 0
-    let visited = Array(n + 1).fill(false)
     for (let i = 1; i <= n; i++) {
         if (visited[i]) continue
         visited[i] = true
-        var hasCycle = false
-        dfs(graph, i, 0, visited)
-        if (!hasCycle) treeCnt++
-    }
 
+        if (dfs(0, i)) {
+            treeCnt++
+        }
+
+
+    }
 
     if (treeCnt === 0) {
-        console.log(`Case ${testCase}: No trees.`)
+        console.log(`Case ${testNum}: No trees.`)
     }
     else if (treeCnt === 1) {
-        console.log(`Case ${testCase}: There is one tree.`)
+        console.log(`Case ${testNum}: There is one tree.`)
     }
     else {
-        console.log(`Case ${testCase}: A forest of ${treeCnt} trees.`)
+        console.log(`Case ${testNum}: A forest of ${treeCnt} trees.`)
     }
-    testCase++
-
+    testNum++
 
 }
 
 
-function dfs(graph, nowNode, prevNode, visited) {
+function dfs(prevNode, nowNode) {
     for (let nextNode of graph[nowNode]) {
-        if (prevNode === nextNode) continue
-        if (visited[nextNode]) {
-            hasCycle = true
-            break
-        }
+        if (nextNode === prevNode) continue
+        if (visited[nextNode]) return false
         visited[nextNode] = true
-        dfs(graph, nextNode, nowNode, visited)
+        if (!dfs(nowNode, nextNode)) return false
     }
 
-
+    return true
 }

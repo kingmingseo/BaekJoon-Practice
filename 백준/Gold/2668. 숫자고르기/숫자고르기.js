65e8737e02d9e928fb52d5464b82f0c8b6b0b194
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-11-30 17:16:15 +0900

    [Gold V] Title: 숫자고르기, Time: 88 ms, Memory: 9420 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.js
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.js
@@ -1,28 +1,22 @@
-let index = require('fs').readFileSync('dev/stdin').toString().trim().split('\n').map(Number)
-let data = index.slice(1)
-let N = index[0]
-// 방향 그래프를 만들기
+let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n').map(Number)
+let N = input[0]
+let data = input.slice(1)
+data.unshift(0)
 
-let graph = Array.from({ length: N + 1 }, () => [])
-
-for (let i = 0; i < N; i++) {
-    graph[i + 1].push(data[i])
-}
-
-
-var ans = []
 function dfs(start, nowNode, visited) {
-    let nextNode = graph[nowNode][0]
+    let nextNode = data[nowNode]
     if (!visited[nextNode]) {
         visited[nextNode] = true
         dfs(start, nextNode, visited)
     }
-    else if (start === nextNode){
+    else if (start === nextNode) {
         ans.push(start)
+        return
     }
-
 }
 
+
+var ans = []
 
 for (let i = 1; i <= N; i++) {
     let visited = Array(N + 1).fill(false)
@@ -30,11 +24,6 @@ for (let i = 1; i <= N; i++) {
     dfs(i, i, visited)
 }
 
-function compare(a, b) {
-    return a - b
-}
-
-ans.sort(compare)
+ans.sort((a, b) => a - b)
 console.log(ans.length)
 console.log(ans.join('\n'))
-

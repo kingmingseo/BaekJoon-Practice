befab63f10eaa68b4696eadc73b45b50379787ea
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2025-12-16 13:22:31 +0900

    [level 1] Title: 체육복, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.13 ms
+메모리: 33.4 MB, 시간: 0.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 21:27:14
+2025년 12월 16일 13:22:32
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,32 +1,47 @@
 function solution(n, lost, reserve) {
-    let canBorrow = 0;
-    lost.sort((a,b)=>(a-b))
-    let temp = 0
-    for (let i = 0; i < lost.length; i++) {
-        if (reserve.indexOf(lost[i]) > -1) {
-            reserve.splice(reserve.indexOf(lost[i]), 1);
-            lost.splice(i, 1);
-            i--; // 배열이 수정되었으므로 인덱스를 다시 확인
-            canBorrow += 1;
-            temp +=1
+    let reserveArray = Array(n+1).fill(1)
+    lost = lost.sort((a,b)=>a-b)
+    reserve = reserve.sort((a,b)=>a-b)
+    reserveArray[0] = 0
+    
+     for(let item of lost){
+        reserveArray[item] = 0
+    }
+    
+    for(let item of reserve){
+        reserveArray[item] += 1
+    }
+    
+   
+    
+
+    for(let item of lost){
+        if(reserveArray[item] === 1){
+            continue
+           
+        }
+        else if(reserveArray[item -1] === 2){
+            reserveArray[item-1] -= 1
+            reserveArray[item] +=1
+           
+        } 
+        else if(reserveArray[item+1]===2){
+            reserveArray[item+1] -=1
+            reserveArray[item] +=1
         }
     }
-
-    for(let i =0; i<lost.length; i++){  
-        
-        //한치 수 작은애 빌려주기
-        if(reserve.indexOf(lost[i]-1)>-1){
-            reserve.splice(reserve.indexOf(lost[i]-1),1)
-            canBorrow+=1;
-            continue
-        //한치 수 큰애 빌려주기
-        }
-        else if(reserve.indexOf(lost[i]+1)>-1){
-            reserve.splice(reserve.indexOf(lost[i]+1),1)
-            canBorrow+=1;
-            continue
-        }
-    }   
- 
-    return n - (lost.length- canBorrow + temp);
+    let answer = 0
+    
+    for(let item of reserveArray){
+        if(item > 0) answer++
+    }
+    
+    return answer
+    
 }
+
+//만약 lost [1,2,4] 인데 reserve가 [1,2,3]
+
+// 1은 자기꺼 입고 
+// 2도 자기꺼 입고
+// 4는 

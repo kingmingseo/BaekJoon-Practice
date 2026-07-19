7ab80536ea28102d86f3da69e52994ee896dee65
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2026-07-20 08:40:49 +0900

    [level 0] Title: 코드 처리하기, Time: 4.00 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String solution(String code) {
+        StringBuilder ret = new StringBuilder();
+        int mode = 0;
+        
+        for(int i=0; i<code.length(); i++){
+            if(mode == 0){
+                if(code.charAt(i) != '1' && i%2==0){
+                    ret.append(code.charAt(i));
+                }
+                else if (code.charAt(i)== '1'){
+                    mode = 1;
+                }
+            }
+            else{
+                if(code.charAt(i) != '1' && i%2==1){
+                    ret.append(code.charAt(i));
+                }
+                else if(code.charAt(i)== '1') {
+                    mode = 0;
+                }
+            }
+        }
+        
+        return ret.length() == 0 ? "EMPTY" : ret.toString();
+    }
+}
+

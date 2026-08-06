a6e7670adb29f1a99cac1c9c6497fe6deadb62de
Minseo Kim <101965138+kingmingseo@users.noreply.github.com>
2026-08-06 18:38:34 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.69 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,38 +1,34 @@
 import java.util.*;
 class Solution {
- 
     public int solution(int[][] board, int[] moves) {
-               
-        List<Integer> result = new ArrayList<>();
         int answer = 0;
-        //크레인을 한번씩 내려보기
+        List<Integer> result = new ArrayList<>();
+        
+        //크레인 내려가는 위치
         for(int move : moves){
             
-            //내린 위치로 한 칸씩 내려가보기
+            //한층씩 내려가기
             for(int i=0; i<board.length; i++){
                 
-                //인형이 있고
+                // 집게에 잡힐 인형이 있고 
                 if(board[i][move-1] != 0){
                     
-                    //현재 결과가 안비어있고 뽑은인형이 마지막 뽑은인형과 같을때 
-                    if(result.size() > 0 &&result.get(result.size()-1)==board[i][move-1]){
-                        result.remove(result.size()-1);   
+                    //result 비어있지 않고 이전이랑 같은경우
+                    if( result.size() > 0 && result.get(result.size()-1)== board[i][move-1] ){
+                        result.remove(result.size()-1);
+                        board[i][move-1] =0;
                         answer+=2;
+                        break;
                     }
                     
-                    //인형을 뽑기만 한 경우 
-                    else{
-                         result.add(board[i][move-1]);
-                    }
-                    board[i][move-1]=0;
+                   
+                    result.add(board[i][move-1]);
+                    board[i][move-1] = 0;
                     break;
-                 
+                    
                 }
-                
             }
         }
         return answer;
     }
-    
-
 }
